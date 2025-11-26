2494384a4de4ed98781ea35b9c6b6fbc33136552
NicoDora <pjh76574734@gmail.com>
2025-11-26 16:22:44 +0900

    refactor/#1: getConfig 함수를 useCallback을 사용해 메모이제이션하도록 개선

--- src/components/organisms/EconomicCalendarWidget.tsx
+++ src/components/organisms/EconomicCalendarWidget.tsx
@@ -1,20 +1,23 @@
-import { memo } from "react";
+import { memo, useCallback } from "react";
 import { TradingViewContainer } from "../molecules/TradingViewContainer";
 
 const EconomicCalendarWidget = () => {
   return (
     <TradingViewContainer
       scriptSrc="https://s3.tradingview.com/external-embedding/embed-widget-events.js"
-      getConfig={(theme) => ({
-        colorTheme: theme,
-        isTransparent: false,
-        locale: "kr",
-        countryFilter:
-          "ar,au,br,ca,cn,fr,de,in,id,it,jp,kr,mx,ru,sa,za,tr,gb,us,eu",
-        importanceFilter: "0,1",
-        width: "100%",
-        height: "100%",
-      })}
+      getConfig={useCallback(
+        (theme) => ({
+          colorTheme: theme,
+          isTransparent: false,
+          locale: "kr",
+          countryFilter:
+            "ar,au,br,ca,cn,fr,de,in,id,it,jp,kr,mx,ru,sa,za,tr,gb,us,eu",
+          importanceFilter: "0,1",
+          width: "100%",
+          height: "100%",
+        }),
+        []
+      )}
     />
   );
 };
--- src/components/organisms/TopStoriesWidget.tsx
+++ src/components/organisms/TopStoriesWidget.tsx
@@ -1,19 +1,22 @@
-import { memo } from 'react';
-import { TradingViewContainer } from '../molecules/TradingViewContainer';
+import { memo, useCallback } from "react";
+import { TradingViewContainer } from "../molecules/TradingViewContainer";
 
 const TopStoriesWidget = () => {
   return (
     <TradingViewContainer
       scriptSrc="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js"
-      getConfig={(theme) => ({
-        displayMode: "regular",
-        feedMode: "all_symbols",
-        colorTheme: theme,
-        isTransparent: false,
-        locale: "kr",
-        width: "100%",
-        height: "100%",
-      })}
+      getConfig={useCallback(
+        (theme) => ({
+          displayMode: "regular",
+          feedMode: "all_symbols",
+          colorTheme: theme,
+          isTransparent: false,
+          locale: "kr",
+          width: "100%",
+          height: "100%",
+        }),
+        []
+      )}
     />
   );
 };
